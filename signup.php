fe890ae4f3ee0f964e21de3f237335e2bf6fcfee
AditiShelke <aditishelke333@gmail.com>
2023-01-01 17:28:38 +0530

    Final

--- signup.php
+++ signup.php
@@ -1,29 +1,13 @@
 <?php
-
-// Connect to the MySQL database
-$host = "localhost";
-$username = "database_username";
-$password = "database_password";
-$dbname = "signup_form";
-$conn = mysqli_connect($host, $username, $password, $dbname);
-
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-
-// Get data from HTML form
+$error = '';
 $name = $_POST['name'];
 $email = $_POST['email'];
 
-// Insert data into MySQL database
-$sql = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
-if (mysqli_query($conn, $sql)) {
+ $data = "$name,$email\n";
+
+  $file = fopen("data.csv", "a");
+  fwrite($file, $data);
+  fclose($file);
+
   echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-
-mysqli_close($conn);
-
 ?>
