b55cf2ea880777c02ee60369325429bdf21d3347
Aditi Jaydeep Shelke <aditishelke333@gmail.com>
2023-05-19 17:22:21 +0530

    Odin project (#2)
    * I
    
    * modified

--- form_db.php
+++ form_db.php
@@ -1,29 +1,40 @@
 <?php
 
-// Connect to the MySQL database
-$host = "localhost";
-$dbname = "form_db";
-$conn = mysqli_connect($host, $dbname);
+// Check if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve form data
+    $name = $_POST['txtName'];
+    $email = $_POST['txtEmail'];
+    $location = $_POST['location'];
+    $member = $_POST['member'];
+    
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "cooking_webpage";
+    // Create a new database connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check for connection errors
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Prepare and execute the SQL query to insert the data into the table
+    $sql = "INSERT INTO register (name, email, location, member) VALUES ('$name', '$email', '$location', '$member')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration Successful!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    // Close the database connection
+    $conn->close();
+}
 // Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error($host, $form_db));
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-
-// Get data from HTML form
-$name = $_POST['name'];
-$email = $_POST['email'];
-$location = $_POST['location'];
-$member = $_POST['member'];
-
-// Insert data into MySQL database
-$sql = "INSERT INTO users ( name, email, location, member) VALUES ('$id','$name', '$email', '$location', '$member')";
-if (mysqli_query($conn, $sql)) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-
-mysqli_close($conn);
-$sql = "SELECT * FROM `users`;";
-
+ echo "Connected successfully";
 ?>
